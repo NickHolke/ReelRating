260fbdc4d6e5784d801c8f3c23656f124474dde7
Nick Holke <nickholke@gmail.com>
2020-01-13 10:07:24 -0800

    Connected to db and created mongoose model

--- database/db_seed.js
+++ database/db_seed.js
@@ -1,6 +1,23 @@
 const unirest = require('unirest');
 const API_KEY = '1aa4b71d59342b08b19dea8b16bcf4aa';
 const baseURL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
+const { save } = require('./db_connection');
+
+save({
+  Title: 'Test Movie',
+  Year: '1969',
+  Runtime: '1 hours',
+  Genre: 'comedy',
+  Director: 'John',
+  Plot: 'A gripping thriller that will knock your socks off.',
+  Awards: 'Emmy',
+  Poster: 'http:posterpsth',
+  InternetMovieDatabase: '98',
+  RottenTomatoes: '12',
+  MetaCritic: '14',
+  imdbRating: '16',
+  reelRating: '43',
+})
 
 let req = unirest("GET", "https://movie-database-imdb-alternative.p.rapidapi.com/");
 
