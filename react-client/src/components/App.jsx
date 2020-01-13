8347598f6cb275abb39f40bba6aab703982be0ea
Nick Holke <nickholke@gmail.com>
2020-01-13 14:48:41 -0800

    App component gets movies from db

--- react-client/src/components/App.jsx
+++ react-client/src/components/App.jsx
@@ -1,6 +1,16 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get('/movies')
+      .then((res) => {
+        setMovies(res.data)
+      })
+      .catch((err) => console.log(err));
+  })
   return <div>hello</div>
 };
 
--- server/index.js
+++ server/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const { getAll } = require('../database/db_connection');
 
 app.use(express.static(__dirname + '/../react-client/dist'));
+
+app.get('/movies', (req, res) => {
+  getAll()
+  .then((data) => {
+    res.send(data);
+  })
+  .catch((err) => {
+    res.writeHead(400);
+    res.send('Issue with db')
+  })
+})
 
 app.listen(port, () => {
   console.log(`Server listening on ${port}`);
