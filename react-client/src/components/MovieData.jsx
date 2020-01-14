c79868de8027d4cb1b0a4876138b0aba2d222030
Nick Holke <nickholke@gmail.com>
2020-01-14 08:54:01 -0800

    Outline of movie data finished

--- react-client/src/components/MovieData.jsx
+++ react-client/src/components/MovieData.jsx
@@ -10,11 +10,44 @@ const Wrapper = styled.div`
   margin-left: 20%;
   margin-top: -10%;
   z-index: 3;
+  font-size: 24px;
+  border-radius: 10px;
+  line-height: 1.4;
+`
+
+const Content = styled.div`
+  padding: 10px;
+`
+
+const ReelRating = styled.div`
+  width: 100px;
+  margin: auto;
+  text-align: center;
+  font-size: 30px;
+`
+
+const Image = styled.img`
+  float: right;
+  height: 310px;
+  width: 240px;
 `
 
 const MovieData = ({ movie, toggleData }) => (
   <Wrapper onClick={() => toggleData()}>
-    {movie.Title}
+    <Content>
+      <Image src={movie.Poster}></Image>
+      <ReelRating>
+        ReelRating: {movie.reelRating}
+      </ReelRating>
+      RottenTomatoes: {movie.RottenTomatoes}<br/>
+      IMDB: {movie.imdbRating}<br/>
+      MetaCritic: {movie.MetaCritic}<br/>
+      Title: {movie.Title}<br/>
+      Year: {movie.Year}<br/>
+      Genre: {movie.Genre}<br/>
+      Director: {movie.Director}<br/>
+      Plot: {movie.Plot}
+    </Content>
   </Wrapper>
 )
 
